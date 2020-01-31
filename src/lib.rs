81dee22f884fda0e8b8ca6ef70b34a3b008f5783
cthwaite <28732700+cthwaite@users.noreply.github.com>
2020-01-31 12:43:45 +0000

    feat: Add pyo3 example

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use pyo3::prelude::*;
+
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+/// Format the sum of two numbers as a string.
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+
+    Ok(())
+}
